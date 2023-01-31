1de1950a03a2875eb70f74c5ae6434993f64b5ab
mattlytle789 <mbl31@pitt.edu>
2023-01-31 10:38:26 -0500

    Fixing initial files

--- Game.cpp/Game.cpp.ino
+++ Game.cpp/Game.cpp.ino
@@ -1,6 +1,24 @@
 /* 
  *  Main code for the blackjack game
  */
+#include "src/Player.h"
+
+// Variable Declarations
+// Game state enumeration
+enum State_Type {pregame, dealing, playerAction, dealerAction, gameOver, clearTable};
+State_Type gameState = pregame; // Current state of the game :: initialized to Pregame state
+
+// Action types enumeration
+enum Action_Type {hit, stand};
+Action_Type actionSelected; // Action the player or dealer has chose to perform
+
+// Player objects
+Player player1; 
+Player player2;
+Player player3; 
+Player dealer; 
+Player *playerList;
+
 
 void setup() {
   // put your setup code here, to run once:
