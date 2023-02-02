531d3098aa217c255f8df2432ab0027871de60ef
Matt Lytle <mbl31@pitt.edu>
2023-02-02 13:07:09 -0500

    messing with file management

--- Game.cpp/Game.cpp.ino
+++ Game.cpp/Game.cpp.ino
@@ -2,6 +2,7 @@
  *  Main code for the blackjack game
  */
 #include "src/Player.h"
+//#include "src/InterruptMethods.h"
 
 // Pin constants
 const int buttonPlayer1Play = 1;
@@ -121,31 +122,31 @@ void loop() {
 }
 
 void player1PlayButton() {
-  player1PlayButtonState = digitalRead(buttonPlayer1Play);
-  if (player1PlayButtonState == 1) {
-    player1 = Player(1);
-    playerList[1] = player1;
-  }
+    player1PlayButtonState = digitalRead(buttonPlayer1Play);
+    if (player1PlayButtonState == 1) {
+        player1 = Player(1);
+        playerList[1] = player1;
+    }
 }
 
 void player2PlayButton() {
-  player2PlayButtonState = digitalRead(buttonPlayer2Play);
-  if (player2PlayButtonState == 1) {
-    player2 = Player(2);
-    playerList[2] = player2;
-  }
+    player2PlayButtonState = digitalRead(buttonPlayer2Play);
+    if (player2PlayButtonState == 1) {
+        player2 = Player(2);
+        playerList[2] = player2;
+    }
 }
 
 void player3PlayButton() {
-  player3PlayButtonState = digitalRead(buttonPlayer3Play);
-  if (player3PlayButtonState == 1) {
-    player3 = Player(3);
-    playerList[3] = player3;
-  }
+    player3PlayButtonState = digitalRead(buttonPlayer3Play);
+    if (player3PlayButtonState == 1) {
+        player3 = Player(3);
+        playerList[3] = player3;
+    }
 }
 
 void dealButton() {
-  dealButtonState = digitalRead(buttonDeal);
+    dealButtonState = digitalRead(buttonDeal);
 }
 
 void player1StandButton() {
@@ -153,25 +154,25 @@ void player1StandButton() {
 }
 
 void player2StandButton() {
-  
+
 }
 
 void player3StandButton() {
-  
+
 }
 
 void player1HitButton() {
-  
+
 }
 
 void player2HitButton() {
-  
+
 }
 
 void player3HitButton() {
-  
+
 }
 
 void clearButton() {
-  
+
 }
