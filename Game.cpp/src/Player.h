ebdc7a654db4c60383a449f1c4533ed3e9f1f26d
Matt Lytle <mbl31@pitt.edu>
2023-02-03 15:36:37 -0500

    overloading == and != operator

--- Game.cpp/src/Player.h
+++ Game.cpp/src/Player.h
@@ -8,4 +8,25 @@ class Player {
         Player(int num) {
             number = num;
         }
+        // getter methods
+        int getNumber() {
+            return number;
+        }
+        // mutator methods
+        void setNumber(int num) {
+            number = num;
+        }
+        // overloading operators
+        bool operator==(Player& P) {
+            if (this->number == P.getNumber()) {
+                return true;
+            }
+            return false;
+        }
+        bool operator!=(Player& P) {
+            if (this->number != P.getNumber()) {
+                return true;
+            }
+            return false;
+        }
 };
