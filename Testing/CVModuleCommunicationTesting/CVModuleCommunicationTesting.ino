f6006582af60f873b5810da824f66ff2a5773f29
Matt Lytle <mbl31@pitt.edu>
2023-03-09 12:07:39 -0500

    wrote code to test communication with CV module

---
+++ Testing/CVModuleCommunicationTesting/CVModuleCommunicationTesting.ino
@@ -0,0 +1,21 @@
+const int controllerOut = 13;
+const int CVin = A0;
+
+volatile int input = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(CVin, INPUT);
+  pinMode(controllerOut, OUTPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(controllerOut, HIGH);
+  input = analogRead(CVin);
+  digitalWrite(controllerOut, LOW);
+  Serial.println(input);
+  delay(500);
+}
