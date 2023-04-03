73c9e7aa946bebd15e77735e1388d2cd9ecc6809
Matt Lytle <mbl31@pitt.edu>
2023-04-03 15:13:09 -0400

    changed pinout configuration

--- Game.cpp/src/Constants.h
+++ Game.cpp/src/Constants.h
@@ -2,14 +2,17 @@
 #define player2PlayButton 5 // Player 2 play button, pin 5
 #define player3PlayButton 6 // Player 3 play button, pin 6
 #define dealButton 9 // Deal button, pin 8
-//#define clearButton 9 // clear button, pin 9
-#define player1ControllerPower 10 // Signal to power player 1 controller, pin 10
-#define player2ControllerPower 11 // Signal to power player 2 controller, pin 11
+#define player1ControllerPower A0 // Signal to power player 1 controller, pin 10
+#define player2ControllerPower A1 // Signal to power player 2 controller, pin 11
 #define player3ControllerPower 12 // Signal to power player 3 controller, pin 12
 #define hitButton 2 // Hit button, pin 1
-#define standButton 3 // Stand button, pin 2
-//#define cvModuleOutputSignal 7 // Output signal to the CV module, pin 7
-//#define cvModuleInputSignal A0 // Input signal from the CV module, pin A0
+#define standButton A2 // Stand button, pin 2
+#define CVout 7 // output signal to CV module, pin 7
+#define CVin 8 // input signal from CV module, pin 8
+#define enMotor1 3 // Signal to power motor 1 base motor, pin 3
+#define enMotor2 10 // Signal to power motor 2 arm motor, pin 10
+#define enMotor3 11 // Signal to power motor 3 height motor, pin 11
+#define motorPolarity A3 // Signal to control polarity of motors, pin A3
 
 // Pin 7 and 13 free digital pins
 // A0,A1,A2,A3,A6,A7 free analog pins
