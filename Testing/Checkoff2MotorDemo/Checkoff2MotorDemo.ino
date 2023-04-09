916520fab6075caa69204810fa9fe43568b997f5
Matt Lytle <mbl31@pitt.edu>
2023-04-09 17:40:00 -0400

    added third motor to test code and first encoder

--- Testing/Checkoff2MotorDemo/Checkoff2MotorDemo.ino
+++ Testing/Checkoff2MotorDemo/Checkoff2MotorDemo.ino
@@ -3,6 +3,10 @@ const int en1 = 3;
 const int en2 = 10;
 const int en3 = 11;
 const int startButton = 5;
+const int receiver = 13;
+volatile int receiverState = 0;
+int receiverCount = 0;
+bool readFlag = true;
 
 void setup() {
   // put your setup code here, to run once:
@@ -11,6 +15,11 @@ void setup() {
   pinMode(en1, OUTPUT);
   pinMode(en2, OUTPUT);
   pinMode(en3, OUTPUT);
+  pinMode(receiver, INPUT_PULLUP);
+
+  PCICR |= B00000001;
+  PCMSK0 |= B00100000;
+
   Serial.begin(9600);
 }
 
@@ -20,20 +29,27 @@ void loop() {
   if (digitalRead(startButton) == 1) {
     digitalWrite(v1, HIGH); // HIGH =  :: LOW =
     analogWrite(en1, 0);
-    analogWrite(en2, 255); 
-    delay(2000);
+    analogWrite(en2, 220); 
+    analogWrite(en3, 0); 
+    while (receiverCount < 1) {
+      Serial.println(receiverCount);
+    }
+    //delay(2000);
     digitalWrite(v1, LOW);
     analogWrite(en1, 0);
     analogWrite(en2, 0);
+    analogWrite(en3, 0);
     delay(3000);
     // Turn Base Motor opposite direction
     digitalWrite(v1, LOW);
     analogWrite(en1, 0);
-    analogWrite(en2, 255);
-    delay(2000);
+    analogWrite(en2, 210);
+    analogWrite(en3, 0);
+    delay(750);
     digitalWrite(v1, HIGH);
     analogWrite(en1, 0);
     analogWrite(en2, 0);
+    analogWrite(en3, 0);
 
     delay(3000);
 
@@ -41,19 +57,55 @@ void loop() {
     digitalWrite(v1, HIGH); // HIGH =  :: LOW =
     analogWrite(en1, 255);
     analogWrite(en2, 0); 
-    delay(2000);
+    analogWrite(en3, 0);
+    delay(1000);
     digitalWrite(v1, LOW);
     analogWrite(en1, 0);
     analogWrite(en2, 0);
+    analogWrite(en3, 0);
     delay(3000);
     // Turn Base Motor opposite direction
     digitalWrite(v1, LOW);
-    analogWrite(en1, 150);
+    analogWrite(en1, 255);
     analogWrite(en2, 0);
-    delay(2000);
+    analogWrite(en3, 0);
+    delay(1000);
     digitalWrite(v1, HIGH);
     analogWrite(en1, 0);
     analogWrite(en2, 0);
+    analogWrite(en3, 0);
 
+    delay(3000);
+
+    // turn height motor
+    digitalWrite(v1, HIGH); // HIGH =  :: LOW =
+    analogWrite(en1, 0);
+    analogWrite(en2, 0); 
+    analogWrite(en3, 100);
+    delay(600);
+    digitalWrite(v1, LOW);
+    analogWrite(en1, 0);
+    analogWrite(en2, 0);
+    analogWrite(en3, 0);
+    delay(3000);
+    // Turn height Motor opposite direction
+    digitalWrite(v1, LOW);
+    analogWrite(en1, 0);
+    analogWrite(en2, 0);
+    analogWrite(en3, 100);
+    delay(400);
+    digitalWrite(v1, HIGH);
+    analogWrite(en1, 0);
+    analogWrite(en2, 0);
+    analogWrite(en3, 0);
+
+  }
+}
+
+ISR (PCINT0_vect) {
+  receiverState = digitalRead(receiver);
+  if (receiverState == 1) {
+    Serial.println("check");
+    receiverCount++;
   }
 }
